c0aa3c15e8eec17551dd5abca2324d462641a7a7
shehabrakib <shehabprogram128@gmail.com>
2022-08-22 21:44:43 +0600

    second commit

--- C213078.cpp
+++ C213078.cpp
@@ -5,39 +5,16 @@ class Product{
 public:
     int product_id;
     string product_category,product_name;
-    void read_product()
-    {
-        ifstream in("productIN.txt");
-        in>>product_id;
-        in.ignore();
-        getline(in,product_category);
-        getline(in,product_name);
-        in.close();
-    }
-    void write_product()
-    {
-        ofstream out("productOut.txt");
-        out<<"ProductID : "<<product_id<<endl;
-        out<<"Product Category : "<<product_category<<endl;
-        out<<"Product Name : "<<product_name<<endl;
-    }
+    void read_product();
+    void write_product();
+    
 };
 class soldProduct : public Product
 {
 public:
     int customer_id, price;
-    void read_sold_product()
-    {
-        ifstream in("customerIN.txt");
-        in>>customer_id>>price;
-        in.close();
-    }
-    void write_sold_product()
-    {
-        ofstream out("customerOut.txt");
-        out<<"CustomerID : "<<customer_id<<endl;
-        out<<"Price : "<<price<<" $";
-    }
+    void read_sold_product();
+    void write_sold_product();
 };
 class boughtProduct : public Product
 {
@@ -45,22 +22,58 @@ public:
     int buyer_id,price;
     string buyer_name;
 
-    void read_bought_product()
-    {
-        ifstream in("buyerIN.txt");
-        in>>buyer_id>>price;
-        in.ignore();
-        getline(in,buyer_name);
-        in.close();
-    }
-    void write_bought_product()
-    {
-        ofstream out("buyerOut.txt");
-        out<<"BuyerID : "<<buyer_id<<endl;
-        out<<"Buyer Name : "<<buyer_name<<endl;
-        out<<"Price : "<<price<<" $"<<endl;
-    }
+    void read_bought_product();
+    void write_bought_product();
 }; 
+void Product :: read_product()
+{
+    ifstream in("productIN.txt");
+    in>>product_id;
+    in.ignore();
+    getline(in,product_category);
+    getline(in,product_name);
+    in.close();
+}
+void Product :: write_product()
+{
+    ofstream out("productOut.txt");
+    out<<"ProductID : "<<product_id<<endl;
+    out<<"Product Category : "<<product_category<<endl;
+    out<<"Product Name : "<<product_name<<endl;
+}
+void soldProduct :: read_sold_product()
+{
+    ifstream in("customerIN.txt");
+    in>>customer_id>>price;
+    in.close();
+}
+void soldProduct :: write_sold_product()
+{
+    ofstream out("customerOut.txt");
+    out<<"ProductID : "<<product_id<<endl;
+    out<<"Product Category : "<<product_category<<endl;
+    out<<"Product Name : "<<product_name<<endl;
+    out<<"CustomerID : "<<customer_id<<endl;
+    out<<"Selling Price : "<<price<<" $";
+}
+void boughtProduct :: read_bought_product()
+{
+    ifstream in("buyerIN.txt");
+    in>>buyer_id>>price;
+    in.ignore();
+    getline(in,buyer_name);
+    in.close();
+}
+void boughtProduct :: write_bought_product()
+{
+    ofstream out("buyerOut.txt");
+    out<<"ProductID : "<<product_id<<endl;
+    out<<"Product Category : "<<product_category<<endl;
+    out<<"Product Name : "<<product_name<<endl;
+    out<<"BuyerID : "<<buyer_id<<endl;
+    out<<"Buyer Name : "<<buyer_name<<endl;
+    out<<"Buying Price : "<<price<<" $"<<endl;
+}
 
 
  
@@ -71,9 +84,11 @@ int main()
     A.write_product();
     soldProduct B;
     B.read_sold_product();
+    B.read_product();
     B.write_sold_product();
     boughtProduct C;
     C.read_bought_product();
+    C.read_product();
     C.write_bought_product();
     return 0;
 }
